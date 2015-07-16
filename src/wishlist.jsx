d3854f37e48c56fa2350ba899f9bf96d93446529
resendefreitas <guilherme@candle.life>
2015-07-16 08:58:08 -0300

    include component 'Menu' in 'wishlist' homepage

--- src/wishlist.jsx
+++ src/wishlist.jsx
@@ -1,4 +1,6 @@
 import storefront from 'storefront';
 import Hello from './components/Hello';
+import Menu from './components/header/Menu';
 
+storefront.export('WishlistPage@vtex.wishlist', Menu);
 storefront.export('WishlistPage@vtex.wishlist', Hello);
