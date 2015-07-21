dc91c5704a747be3a71b0d013f77c6c29882e66b
bellacorbo <bella.corbo@gmail.com>
2015-07-21 19:27:21 -0300

    updates header style

--- src/components/header/Brand.jsx
+++ src/components/header/Brand.jsx
@@ -4,8 +4,8 @@ let Brand = React.createClass({
 
   render() {
     return (
-      <div className="col-xs-4 navbar-brand row container-fluid ">
-        <p className="text-center">Logo</p>
+      <div className="col-xs-4 ">
+        <p className="text-center navbar-brand">Logo</p>
       </div>
     );
   }
