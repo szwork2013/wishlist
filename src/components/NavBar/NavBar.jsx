4546b311c3e09f34c5f22711e9642661ed049b8e
bellacorbo <bella.corbo@gmail.com>
2015-07-28 17:56:52 -0300

    fixes syntax

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -8,11 +8,11 @@ let NavBar = React.createClass({
 
   render: function(){
     return (
-      <div class="wishlistnav col-xs-12">
-          <Name/>
-          <WishlistIcon/>
-          <div class="col-xs-4"></div>
-          <div class="col-xs-4"></div>
+      <div className="wishlistnav col-xs-12">
+          <div className="col-xs-6"><Name/></div>
+          <div className="col-xs-6"><WishlistIcon/></div>
+          <div className="col-xs-4"></div>
+          <div className="col-xs-4"></div>
       </div>
     );
   }
