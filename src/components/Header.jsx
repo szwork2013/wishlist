1b5779dbb887acb76685f785b85dd0dd54290051
bellacorbo <bella.corbo@gmail.com>
2015-07-28 16:04:46 -0300

    alters header strcuture

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,13 +3,14 @@ import React from 'react';
 import Menu from './header/Menu';
 import Brand from './header/Brand';
 import GoToCart from './header/GoToCart';
+import style from '../../styles/WishlistPage.less';
 
 let Header = React.createClass({
 
   render: function(){
     return (
-      <nav className="col-md-12 navbar navbar-default">
-        <div className="container-fluid row">
+      <nav className="navigation col-xs-12 navbar navbar-default">
+        <div className="container-fluid row top-buttons">
           <Menu/>
           <Brand/>
           <GoToCart/>
