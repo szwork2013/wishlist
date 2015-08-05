755ba369f7dc68fa82bbb49e5e29a584694a1904
bellacorbo <bella.corbo@gmail.com>
2015-08-05 14:34:40 -0300

    alters SKU Image component

--- src/components/WishlistItem/Picture.jsx
+++ src/components/WishlistItem/Picture.jsx
@@ -5,8 +5,10 @@ let Picture = React.createClass({
 
   render() {
     return (
-        <div className="col-xs-12">
-          <img src="http://i717.photobucket.com/albums/ww172/louisearaujo/23cheerful-cat.jpg" className="img-thumbnail wishlistphoto"/>
+        <div className="col-xs-4 photocontainer">
+          <div className=" wishlistphoto">
+            <img src="http://i717.photobucket.com/albums/ww172/louisearaujo/23cheerful-cat.jpg" className="img-thumbnail"/>
+          </div>
         </div>
     );
   }
