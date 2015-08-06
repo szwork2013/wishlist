74129e2b659a4ebc63e7a197cb225fc5f988c34f
bellacorbo <bella.corbo@gmail.com>
2015-08-06 15:09:29 -0300

    creates simple documentation for WishlistIcon.jsx

--- src/components/NavBar/WishlistIcon.jsx
+++ src/components/NavBar/WishlistIcon.jsx
@@ -1,3 +1,11 @@
+/***** WishlistIcon.jsx *******/
+/*
+* Descrição: Componente que do ícone da Wishlist na página.
+*
+*  Componentes relacionados:
+*     - WishlistIcon.less: descreve as classes de CSS usadas no componente WishlistIcon.jsx.
+*
+*/
 import React from 'react';
 import style from 'styles/NavBar.less'; // eslint-disable-line
 
