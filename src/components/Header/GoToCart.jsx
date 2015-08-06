6a5ec35ff5d3cdddef6558ac8c10d0c74a330e62
bellacorbo <bella.corbo@gmail.com>
2015-08-06 17:21:53 -0300

    changes header grid structure

--- src/components/Header/GoToCart.jsx
+++ src/components/Header/GoToCart.jsx
@@ -8,9 +8,9 @@ let GoToCart = React.createClass({
 
   render() {
     return (
-      <div className="col-xs-3">
+      <div className="col-xs-4">
         <button type="button"
-        className="buttons glyphicon glyphicon-shopping-cart btn btn-default col-xs-5 buttom"
+        className=" buttons glyphicon glyphicon-shopping-cart btn btn-default col-xs-5 buttom"
         onClick={this.handleClick}></button>
       </div>
     );
--- src/components/Header/Menu.jsx
+++ src/components/Header/Menu.jsx
@@ -11,7 +11,7 @@ let Menu = React.createClass({
 
   render() {
     return (
-      <div className="col-xs-3 menu">
+      <div className="col-xs-4 menu">
         <button
         className="glyphicon glyphicon-menu-hamburger btn btn-default buttom"
         onClick={this.handleClick}></button>
