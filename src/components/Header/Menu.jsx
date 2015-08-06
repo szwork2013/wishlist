c6a68b41df6d21aa119424e1a7edd5a56ea8e0a6
bellacorbo <bella.corbo@gmail.com>
2015-08-06 14:33:40 -0300

    creates simple documentation for Menu.jsx

--- src/components/Header/Menu.jsx
+++ src/components/Header/Menu.jsx
@@ -1,3 +1,12 @@
+/***** Menu.jsx *******/
+/*
+* Descrição: Implementa o botão de acessar o menu
+*     da página da Wishlist.
+*
+*  Componentes relacionados:
+*     - Menu.less: descreve as classes de CSS do componente Menu.jsx
+*
+*/
 import React from 'react';
 import style from '../../styles/Menu.less'; // eslint-disable-line
 
@@ -10,7 +19,7 @@ let Menu = React.createClass({
     return (
       <div className="col-xs-3 menu">
         <button
-        className="glyphicon glyphicon-menu-hamburger btn btn-default buttom" 
+        className="glyphicon glyphicon-menu-hamburger btn btn-default buttom"
         onClick={this.handleClick}></button>
       </div>
     );
