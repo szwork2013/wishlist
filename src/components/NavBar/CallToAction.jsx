3a1c0a71d357c91bfb6d69024edcd28ac1b16bc4
Guigs <guilherme.freitas@vtex.com.br>
2015-08-06 12:01:23 -0300

    Adds email retrival structure

--- src/components/NavBar/CallToAction.jsx
+++ src/components/NavBar/CallToAction.jsx
@@ -6,19 +6,22 @@ let CallToAction = React.createClass({
   handleClick(){
       console.log('Abre o modal para login. VETX ID');
   },
+  autentica() {
+    document.getElementById('loga').className='col-xs-offset-3';
+  },
 
   render() {
     return (
-    /*  <div style={{ height: 60}}>
-        <Popover placement='bottom' positionRight={80} className='popoverColor popoverPosition'>
-        Faça <a href="#" className="alert-link" onClick={this.handleClick}>login</a> para lembrarmos de você</Popover>
-      </div> */
-
-      <div className="alert alert-info" role="alert">
-        <p><small>Quer acessar sua Wishlist de qualquer lugar?</small>
-        <br/><small><a href="#" className="alert-link">Informe o seu email.</a></small></p>
+      <div>
+        <div className="alert alert-info" role="alert" onClick={this.autentica}>
+          <p><small>Quer acessar sua Wishlist de qualquer lugar?</small>
+          <br/><small><a href="#" className="alert-link">Informe o seu email.</a></small></p>
+        </div>
+        <div className="hidden" id="loga">
+          <input placeholder="loga ae"></input>
+          <button className="btn btn-submit">entrar</button>
+        </div>
       </div>
-
     );
   }
 });
