0e80d9245119b2623752fbe8185be5aacb1703bb
bellacorbo <bella.corbo@gmail.com>
2015-08-07 16:11:34 -0300

    changes Send button in Footer

--- src/components/Footer/Send.jsx
+++ src/components/Footer/Send.jsx
@@ -24,6 +24,9 @@ let Send = React.createClass({
 
     console.log('Lista:', mail);
     console.log('IDs armazenados com sucesso.');
+
+    // let email = "caso seja necessario definir um email, escrever aqui";
+    // document.write("<a href = mailto:"+email+">"+lista+"</a>"); --> metodo possivelmente util eventualmente
     
     let subject = 'Minha Wishlist';
     let body = 'Produtos: ' + lista;
@@ -36,13 +39,12 @@ let Send = React.createClass({
 
   render() {
     return (
-      <div className="col-xs-4 navbar-form navbar-left row container-fluid">
+      <div>
         <a href="#" onclick="javascript:window.location='mailto:'+this.email+'+'&body='+this.mail">
-        <button type="button"
-        className="btn btn-info"
-        onClick={this.handleClickMail}>
-          Enviar essa lista
-        </button></a>
+          <button type="button"
+          className=" buttons fa fa-paper-plane-o btn btn-default buttom"
+          onClick={this.handleClickMail}></button>
+        </a>
       </div>
     );
   }
