1e8eea81abbe2473be09868b702ad262d476c781
Luiza Breier <luizabreier21@gmail.com>
2015-08-07 12:11:43 -0300

    fixes padding

--- src/components/WishlistItem/WishlistItem.jsx
+++ src/components/WishlistItem/WishlistItem.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import Picture from './Picture';
 import Price from './Price';
 import AddCart from './AddCart';
@@ -6,9 +7,10 @@ import Description from './Description';
 import WishlistActions from '../../actions/WishlistActions';
 
 let WishlistItem = React.createClass({
+
   render() {
     return (
-      <nav className="wishlistcontent col-xs-12 navbar uk-thumbnail">
+      <nav className="wishlistcontent navbar uk-thumbnail">
         <div className="wishlistlist navbar uk-thumbnail" onClick={this.removeSkuFromWishlist}>
           <Picture sku={this.props.sku}/>
           <div className="col-xs-8 description">
