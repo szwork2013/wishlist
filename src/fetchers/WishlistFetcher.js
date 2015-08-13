645c2e74bbd8c7100e97e12789ca5e47803410c5
Guigs <guilherme.freitas@vtex.com.br>
2015-08-13 16:51:13 -0300

    Fixes request core problem

--- src/fetchers/WishlistFetcher.js
+++ src/fetchers/WishlistFetcher.js
@@ -4,11 +4,7 @@ class WishlistFetcher {
   fetch(email) {
     return request({
       url: `http://api.beta.vtex.com/basedevmkp/masterdata/wishlist/_search?email=${email}`,
-      contentType: 'application/json',
-      crossOrigin: true,//core problems
-      headers: {
-      'X-My-Custom-Header': 'SomethingImportant'//waiting for resources not yet developed
-    }
+      crossOrigin: true
     });
   }
 }
