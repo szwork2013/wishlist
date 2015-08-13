1170a08cf319458259836ea2940b842cb77dbfec
milasd <milasoaresdeoliveira@gmail.com>
2015-08-13 20:59:05 -0300

    Listens to WishlistStore

--- src/components/Catalog/Catalog.jsx
+++ src/components/Catalog/Catalog.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import SkuItem from './SkuItem';
 import CookieManager from '../CookieManager';
 import Header from '../Header/Header';
+import WishlistStore from '../../stores/WishlistStore';
 
 import image1 from '../../images/catalogo.png';
 import image2 from '../../images/catalogo2.png';
@@ -37,9 +38,9 @@ let Catalog = React.createClass({
           price: 270.0
         }
       ]
-    };
+    }
   },
-
+  
   render() {
     return (
       <div className="main-container">
