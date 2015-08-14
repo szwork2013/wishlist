602b68da0aad90fcab0e7634caad8ef3b3d685c0
bellacorbo <bella.corbo@gmail.com>
2015-08-14 13:38:58 -0300

    moves catalog link to brand logo

--- src/components/Header/Brand.jsx
+++ src/components/Header/Brand.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import '../../styles/Brand.less';
+import { Navigation } from 'react-router';
 
 let Brand = React.createClass({
+  mixins:[Navigation],
+
+  handleClick() {
+    this.transitionTo('/wishlist/catalog');
+  },
 
   render() {
     return (
       <div className="col-xs-4 navbar-brand">
-        <img src="http://frontendcarioca.com.br/assets/image/empresas/patrocinio/vtex.png" className="center-block v-wish__site-logo"/>
+        <img src="http://frontendcarioca.com.br/assets/image/empresas/patrocinio/vtex.png" className="center-block v-wish__site-logo" onClick={this.handleClick}/>
       </div>
     );
   }
--- src/components/Header/Menu.jsx
+++ src/components/Header/Menu.jsx
@@ -1,21 +1,14 @@
 import React from 'react';
 
-import { Navigation } from 'react-router';
 import '../../styles/Menu.less';
 
 let Menu = React.createClass({
-  mixins:[Navigation],
-
-  handleClick() {
-    this.transitionTo('/wishlist/catalog');
-  },
-
   render() {
     return (
       <div className="col-xs-4 menu">
         <button
-        className="glyphicon glyphicon-menu-hamburger btn btn-default"
-        onClick={this.handleClick}></button>
+        className="glyphicon glyphicon-menu-hamburger btn btn-default">
+        </button>
       </div>
     );
   }
