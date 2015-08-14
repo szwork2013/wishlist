a3c8cbec262fe77b64f4c1c7149e760f9a38d86d
milasd <milasoaresdeoliveira@gmail.com>
2015-08-13 22:15:25 -0300

    Removes Listening

--- src/components/Catalog/Catalog.jsx
+++ src/components/Catalog/Catalog.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import SkuItem from './SkuItem';
 import CookieManager from '../CookieManager';
 import Header from '../Header/Header';
-import WishlistStore from '../../stores/WishlistStore';
 
 import image1 from '../../images/catalogo.png';
 import image2 from '../../images/catalogo2.png';
