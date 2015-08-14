fc70c092e9534aabbe0198d1822cc90f845b844e
Guigs <guilherme.freitas@vtex.com.br>
2015-08-14 00:17:06 -0300

    Removes catalog actions from WishlistActions

--- src/actions/WishlistActions.js
+++ src/actions/WishlistActions.js
@@ -1,12 +1,8 @@
 import flux from '../flux';
-import WishlistFetcher from '../fetchers/WishlistFetcher'
 
 class WishlistActions {
   addSku(sku) {
 	  this.dispatch(sku);
-  }
-  addList(skus){
-	  this.dispatch(skus);
   }
   removeSku(sku) {
 	  this.dispatch(sku);
@@ -14,18 +10,8 @@ class WishlistActions {
   displayPopover() {
     this.dispatch();
   }
-  searchEmail(email) {
-    WishlistFetcher.fetch(email).then((result)=>{
-      if(result.docs.length === 0){
-        console.log('no matching email');
-      }
-      else{
-        console.log(result.docs[0].skus);
-        this.dispatch(result.docs[0].skus);
-      }
-    }).catch(()=>{
-        console.log('could not access api');
-      });
+  retrieveSkus(skus) {
+    this.dispatch(skus);
   }
 }
 
